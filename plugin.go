b3da53c1d75938c90b87e7fcccfe1dc009b197fd
Takushi HASHIDA <hashida.takushi@jp.fujitsu.com>
2018-07-29 17:00:35 +0900

    work

---
+++ plugin.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	// "crypto/subtle"
+	// "encoding/json"
+	"net/http"
+	"sync/atomic"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+type Configuration struct {
+	Enabled  bool
+	Secret   string
+	UserName string
+}
+
+type Plugin struct {
+	api           plugin.API
+	configuration atomic.Value
+}
+
+func (p *Plugin) OnActivate(api plugin.API) error {
+	p.api = api
+	return p.OnConfigurationChange()
+}
+
+func (p *Plugin) config() *Configuration {
+	return p.configuration.Load().(*Configuration)
+}
+
+func (p *Plugin) OnConfigurationChange() error {
+	var configuration Configuration
+	err := p.api.LoadPluginConfiguration(&configuration)
+	p.configuration.Store(&configuration)
+	return err
+}
+
+func (p *Plugin) SlackAttachment() (*model.SlackAttachment, error) {
+	return &model.SlackAttachment{
+		Fallback: "Fallback",
+		Color:    "#95b7d0",
+		Pretext:  "This is Pre-text",
+		Text:     "This is text",
+	}, nil
+}
+
+func (p *Plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	config := p.config()
+
+	if !config.Enabled || config.Secret == "" || config.UserName == "" {
+		http.Error(w, "This plugin is not configured.", http.StatusForbidden)
+		return
+	}
+
+
+	team, _ := p.api.GetTeamByName("test");
+	channel, _ := p.api.GetChannelByName("test", team.Id);
+	user, _ := p.api.GetUserByUsername(config.UserName);
+
+	attachment, _ := p.SlackAttachment();
+
+	if _, err := p.api.CreatePost(&model.Post{
+		ChannelId: channel.Id,
+		Type:      model.POST_SLACK_ATTACHMENT,
+		UserId:    user.Id,
+		Props: map[string]interface{}{
+			"from_webhook":  "true",
+			"use_user_icon": "true",
+			"attachments":   []*model.SlackAttachment{attachment},
+		},
+	}); err != nil {
+		http.Error(w, err.Message, err.StatusCode)
+	}
+
+	fmt.Fprint(w, "ok.")
+
+}
+
+
